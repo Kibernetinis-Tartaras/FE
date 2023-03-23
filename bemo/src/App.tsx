2430e5e5957b3a34cfefd7ada1b410f7a8696e3f
Tedas <tedodarbo@gmail.com>
2023-03-23 12:39:49 +0200

    Main routes

--- bemo/src/App.tsx
+++ bemo/src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Sv
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={null} />
+          <Route path="/signup" element={null} />
+          <Route path="/main" element={null} />
+          <Route path="/profile" element={null} />
+          <Route path="/" element={null} />
+          <Route path="/*" element={null} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
